0e17c0d9a41c7c8e9cac2d3506220d2d4647bd70
Amy <amynedervelt@gmail.com>
2017-07-11 20:17:02 -0500

    Added first JS

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+window.onload = function(event) {
+  console.log('Hi mom');
+  console.log(document);
+  let ul = document.createElement('ul');
+  console.log(ul);
+
+  document.getElementById('list').innerHTML =
+  'i like dogs';
+
+
+
+
+
+}
